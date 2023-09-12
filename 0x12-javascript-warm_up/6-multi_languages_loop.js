e9804353c5981a8649c3af5f626d4caf5bc4eaa7
YassineDeriouch <yderouich14@gmail.com>
2023-09-12 05:57:58 +0000

    6-multi_languages_loop.js typo fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node 
-for (let i of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+for (let i of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
   console.log(i);
 }
