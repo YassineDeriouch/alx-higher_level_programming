c28b2c629bb01d5a31f05b093d21ed83783dd494
YassineDeriouch <yderouich14@gmail.com>
2023-09-12 08:15:04 +0000

    8-square.js fix1

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,8 +1,9 @@
-//#!/usr/bin/node
-if (isNaN(parseInt(process.argv[2]))) {
-    console.log('Missing size');
-  } else if (parseInt(process.argv[2]) > 0) {
-    for (let i = 0; i < parseInt(process.argv[2]); i++) {
-      console.log('X'.repeat(parseInt(process.argv[2])));
-    }
+#!/usr/bin/node
+const size = parseInt(process.argv[2]);
+if (!isNaN(size)) {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
   }
+} else {
+  console.log('Missing size');
+}
