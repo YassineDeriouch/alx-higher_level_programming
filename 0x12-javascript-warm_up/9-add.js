e46066e47ace7c4288c8b69060d9cb47c541ced2
YassineDeriouch <yderouich14@gmail.com>
2023-09-12 22:39:08 +0000

    9-add.js fix5

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,7 +2,7 @@
 function add (a, b) {
   return parseInt(a) + parseInt(b)
 }
-const arg1 = parseInt(process.argv[2])
-const arg2 = parseInt(process.argv[3])
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
 
-console.log(add(arg1, arg2))
+console.log(add(arg1, arg2));
