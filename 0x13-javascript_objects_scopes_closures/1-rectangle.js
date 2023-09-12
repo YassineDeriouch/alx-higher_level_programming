eb4dc8c25ac6e9e3a7c826b66d7febb09c4744dd
YassineDeriouch <yderouich14@gmail.com>
2023-09-12 22:58:22 +0000

    1. Rectangle #1 fix1

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-class Rectangle {
-    constructor(w, h) {
-      this.width = w;
-      this.height = h;
+module.exports = class Rectangle {
+    constructor (w, h) {
+      if (!(w < 1) || (h < 1)) {
+        this.width = w;
+        this.height = h;
+      }
     }
-  }
-  
+  };
