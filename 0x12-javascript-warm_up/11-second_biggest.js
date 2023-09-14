7488d16290eb65b20a66462454f88948ad9eeb1d
YassineDeriouch <yderouich14@gmail.com>
2023-09-14 00:04:42 +0000

    11-second_biggest.js fix00

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
+const argsArray = Array.from(arguments).map(Number);
+if (argsArray.length <= 1) {
   console.log(0);
-} else {
-  const args = process.argv.map(Number)
-    .slice(2, process.argv.length)
-    .sort((a, b) => a - b);
-  console.log(args[args.length - 2]);
 }
+const sortedArgs = argsArray.sort((a, b) => b - a);
+console.log(sortedArgs[1]);
