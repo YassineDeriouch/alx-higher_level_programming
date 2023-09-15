e625580175fdd459eee2be22e45f98c9555cc7d2
YassineDeriouch <yderouich14@gmail.com>
2023-09-14 23:45:48 -0700

    9. Full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,6 +5,5 @@ CREATE TABLE IF NOT EXISTS second_table (
 	score int
 	
 );
-
 -- insert data to second table
 INSERT INTO second_table VALUES (1, 'John', 10),(2,'Alex', 3),(3, 'Bob', 14),(4, 'George', 8);
