8d29d26b9168a05abd6cc9ff4d0cf1dfd5db6769
YassineDeriouch <yderouich14@gmail.com>
2023-09-17 08:37:42 -0700

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Python script to lists all states from the database hbtn_0e_0_usa"""
+"""Python script to list all states from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 import sys
