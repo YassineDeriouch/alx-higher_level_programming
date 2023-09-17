d3f5ae48303df40cd2a27026e29d8164e7d990fd
YassineDeriouch <yderouich14@gmail.com>
2023-09-17 09:16:07 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
 """
-python script that takes input in argument and displays all matching names of states
+python script that
+takes input in argument
+and displays all
+matching names of states
 """
 
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        port=3306,
+        passwd=argv[2],
+        db=argv[3]
+    )
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states \
-                 WHERE name LIKE BINARY '{}' \
-                 ORDER BY states.id ASC".format(argv[4]))
+    cur.execute("SELECT * FROM states "
+                "WHERE name LIKE BINARY '{}' "
+                "ORDER BY states.id ASC".format(argv[4]))
     rows = cur.fetchall()
 
     for row in rows:
