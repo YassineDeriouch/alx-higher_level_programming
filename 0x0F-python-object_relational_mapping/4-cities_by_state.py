48dbd76389d2e88c1bffc2264b9e481726d534da
YassineDeriouch <yderouich14@gmail.com>
2023-09-17 09:47:38 -0700

    4-cities_by_state.py fix.

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,41 +1,37 @@
 #!/usr/bin/python3
+"""
+This script lists all cities from
+the database `hbtn_0e_4_usa`.
+"""
 
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <database>")
-    else:
-        username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    """
+    Access to the database and get the cities
+    from the database.
+    """
 
-        try:
-            db = MySQLdb.connect(
-                host="localhost",
-                user=username,
-                port=3306,
-                passwd=password,
-                db=database
-            )
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-            cur = db.cursor()
+    with db.cursor() as cur:
+        cur.execute("""
+            SELECT
+                cities.id, cities.name, states.name
+            FROM
+                cities
+            JOIN
+                states
+            ON
+                cities.state_id = states.id
+            ORDER BY
+                cities.id ASC
+        """)
 
-            cur.execute("""
-                SELECT cities.id, cities.name, states.name
-                FROM cities
-                JOIN states ON cities.state_id = states.id
-                ORDER BY cities.id ASC;
-            """)
+        rows = cur.fetchall()
 
-            rows = cur.fetchall()
-
-            for row in rows:
-                print(row)
-
-        except MySQLdb.Error as e:
-            print("MySQL Error:", e)
-        finally:
-            if 'cur' in locals():
-                cur.close()
-            if 'db' in locals():
-                db.close()
+    if rows is not None:
+        for row in rows:
+            print(row)
