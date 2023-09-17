92d547e3be391b741510e5b949dfc9ebdd5c26a3
YassineDeriouch <yderouich14@gmail.com>
2023-09-17 09:32:34 -0700

    3-my_safe_filter_states.py fix

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,35 +1,35 @@
 #!/usr/bin/python3
-
-"""Python script to list all
-states with a name starting
-with letter N in hbtn_0e_0_usa DB
-safe from MySQL injections!
-"""
-
+'''
+that lists all states from database where name matches the argument.
+'''
 import MySQLdb
 from sys import argv
 
+
+def all_states_filter_entry():
+    username, password, database_name, state_name = argv[1:5]
+    state_name = state_name.split(';')[0].strip("'")
+    conn = MySQLdb.connect(
+                           host="localhost",
+                           port=3306,
+                           user=username,
+                           passwd=password,
+                           db=database_name,
+                           charset="utf8"
+                            )
+    cur = conn.cursor()
+    cur.execute("""
+                SELECT * FROM states
+                WHERE states.name = Binary('{}')
+                ORDER BY id ASC
+                """.format(state_name)
+                )
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
 if __name__ == '__main__':
-
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
-
-    with db.cursor() as cur:
-        cur.execute("""
-            SELECT
-                *
-            FROM
-                states
-            WHERE
-                name LIKE BINARY %(name)s
-            ORDER BY
-                states.id ASC
-        """, {
-            'name': argv[4]
-        })
-
-        rows = cur.fetchall()
-
-    if rows is not None:
-        for row in rows:
-            print(row)
+    all_states_filter_entry()
