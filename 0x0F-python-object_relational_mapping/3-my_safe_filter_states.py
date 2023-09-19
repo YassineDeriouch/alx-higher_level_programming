0ee7a1d95f98763fcb4f1b5f5f817c1c11ffac06
YassineDeriouch <yderouich14@gmail.com>
2023-09-18 19:31:07 -0700

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,34 +2,39 @@
 '''
 that lists all states from database where name matches the argument.
 '''
+
 import MySQLdb
-from sys import argv
+import sys
 
 
-def all_states_filter_entry():
-    username, password, database_name, state_name = argv[1:5]
-    state_name = state_name.split(';')[0].strip("'")
-    conn = MySQLdb.connect(
-                           host="localhost",
-                           port=3306,
-                           user=username,
-                           passwd=password,
-                           db=database_name,
-                           charset="utf8"
-                            )
-    cur = conn.cursor()
-    cur.execute("""
-                SELECT * FROM states
-                WHERE states.name = Binary('{}')
-                ORDER BY id ASC
-                """.format(state_name)
-                )
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+def search_states(username, password, database, state_name):
+    try:
+        db = MySQLdb.connect(
+            host="localhost", port=3306,
+            user=username, passwd=password, db=database
+        )
+        cursor = db.cursor()
+        sql_query = "SELECT * FROM states WHERE name=%s ORDER BY id ASC;"
+        cursor.execute(sql_query, (state_name,))
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+        cursor.close()
+        db.close()
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+        sys.exit(1)
 
 
-if __name__ == '__main__':
-    all_states_filter_entry()
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: python script.py <username> <password> <database>"
+              " <state_name>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    search_states(username, password, database, state_name)
