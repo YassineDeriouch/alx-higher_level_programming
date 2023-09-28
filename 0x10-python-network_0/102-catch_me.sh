54b6ba2972094f762f09804269e75ed4fd4c13ae
YassineDeriouch <yderouich14@gmail.com>
2023-09-28 11:45:20 -0700

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # bash script that makes a request to show custom response
-curl -sL 0.0.0.0:5000/catch_me -X PUT -H "Origin:You got me!"
+curl -sLX GET -H "Host: 0.0.0.0:5000" -H "User-Agent: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
